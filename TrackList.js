75c0e8a340d1c88020270e90822f2f4528dfb527
vfranco2 <vladfranco2@gmail.com>
2024-04-20 20:03:16 -0400

    1.1 - Route and data updates

--- TrackList.js
+++ TrackList.js
@@ -1,133 +1,133 @@
 const trackList = [
 	{
-    SongId: 0, SongLink: "musen/songs/citypop/spacescraper.mp3",
-    SongTitle: "SPACE SCRAPER", SongArtist: "TOSHIKI KADOMATSU",
-    SongLength: 330000, SongImage: "musen/songs/covers/spacescraper.jpg"
+    songId: 0, songLink: "musen/songs/citypop/spacescraper.mp3",
+    songTitle: "SPACE SCRAPER", songArtist: "TOSHIKI KADOMATSU",
+    songLength: 330000, songImage: "musen/songs/covers/spacescraper.jpg"
 	},
 	{
-    SongId: 1, SongLink: "musen/songs/citypop/plasticlove.mp3",
-    SongTitle: "PLASTIC LOVE", SongArtist: "竹内 まりや",
-    SongLength: 294000, SongImage: "musen/songs/covers/plasticlove.jpg"
+    songId: 1, songLink: "musen/songs/citypop/plasticlove.mp3",
+    songTitle: "PLASTIC LOVE", songArtist: "竹内 まりや",
+    songLength: 294000, songImage: "musen/songs/covers/plasticlove.jpg"
 	},
 	{
-    SongId: 2, SongLink: "musen/songs/citypop/sunset.mp3",
-    SongTitle: "SUNSET", SongArtist: "BLU-SWING",
-    SongLength: 209000, SongImage: "musen/songs/covers/sunset.jpg"
+    songId: 2, songLink: "musen/songs/citypop/sunset.mp3",
+    songTitle: "SUNSET", songArtist: "BLU-SWING",
+    songLength: 209000, songImage: "musen/songs/covers/sunset.jpg"
 	},
 	{
-    SongId: 3, SongLink: "musen/songs/citypop/baycity.mp3",
-    SongTitle: "BAY CITY", SongArtist: "黄昏の",
-    SongLength: 250000, SongImage: "musen/songs/covers/baycity.jpg"
+    songId: 3, songLink: "musen/songs/citypop/baycity.mp3",
+    songTitle: "BAY CITY", songArtist: "黄昏の",
+    songLength: 250000, songImage: "musen/songs/covers/baycity.jpg"
 	},
 	{
-    SongId: 4, SongLink: "musen/songs/citypop/summerbreeze.mp3",
-    SongTitle: "SUMMER BREEZE", SongArtist: "PIPER",
-    SongLength: 203000, SongImage: "musen/songs/covers/summerbreeze.jpg"
+    songId: 4, songLink: "musen/songs/citypop/summerbreeze.mp3",
+    songTitle: "SUMMER BREEZE", songArtist: "PIPER",
+    songLength: 203000, songImage: "musen/songs/covers/summerbreeze.jpg"
 	},
 	{
-    SongId: 5, SongLink: "musen/songs/citypop/catseye.mp3",
-    SongTitle: "CAT'S EYE", SongArtist: "杏里",
-    SongLength: 189000, SongImage: "musen/songs/covers/timely.jpg"
+    songId: 5, songLink: "musen/songs/citypop/catseye.mp3",
+    songTitle: "CAT'S EYE", songArtist: "杏里",
+    songLength: 189000, songImage: "musen/songs/covers/timely.jpg"
 	},
 	{
-    SongId: 6, SongLink: "musen/songs/jazz/itcouldhappen.mp3",
-    SongTitle: "IT COULD HAPPEN TO YOU", SongArtist: "RYO FUKUI",
-    SongLength: 257000, SongImage: "musen/songs/covers/scenery.jpg"
+    songId: 6, songLink: "musen/songs/jazz/itcouldhappen.mp3",
+    songTitle: "IT COULD HAPPEN TO YOU", songArtist: "RYO FUKUI",
+    songLength: 257000, songImage: "musen/songs/covers/scenery.jpg"
 	},
 	{
-    SongId: 7, SongLink: "musen/songs/lofi/aruarian.mp3",
-    SongTitle: "ARUARIAN DANCE", SongArtist: "NUJABES",
-    SongLength: 252000, SongImage: "musen/songs/covers/champloo.jpg"
+    songId: 7, songLink: "musen/songs/lofi/aruarian.mp3",
+    songTitle: "ARUARIAN DANCE", songArtist: "NUJABES",
+    songLength: 252000, songImage: "musen/songs/covers/champloo.jpg"
 	},
 	{
-    SongId: 8, SongLink: "musen/songs/acid/zenmai.mp3",
-    SongTitle: "ZENMAI", SongArtist: "ススム ヨコタ",
-    SongLength: 250000, SongImage: "musen/songs/covers/fuji.jpg"
+    songId: 8, songLink: "musen/songs/acid/zenmai.mp3",
+    songTitle: "ZENMAI", songArtist: "ススム ヨコタ",
+    songLength: 250000, songImage: "musen/songs/covers/fuji.jpg"
 	},
 	{
-    SongId: 9, SongLink: "musen/songs/lofi/feel.mp3",
-    SongTitle: "FEEL", SongArtist: "吉村弘",
-    SongLength: 274000, SongImage: "musen/songs/covers/green.jpg"
+    songId: 9, songLink: "musen/songs/lofi/feel.mp3",
+    songTitle: "FEEL", songArtist: "吉村弘",
+    songLength: 274000, songImage: "musen/songs/covers/green.jpg"
 	},
 	{
-    SongId: 10, SongLink: "musen/songs/lofi/green.mp3",
-    SongTitle: "GREEN", SongArtist: "吉村弘",
-    SongLength: 314000, SongImage: "musen/songs/covers/green.jpg"
+    songId: 10, songLink: "musen/songs/lofi/green.mp3",
+    songTitle: "GREEN", songArtist: "吉村弘",
+    songLength: 314000, songImage: "musen/songs/covers/green.jpg"
 	},
 	{
-    SongId: 11, SongLink: "musen/songs/lofi/mist.mp3",
-    SongTitle: "MIST", SongArtist: "吉村弘",
-    SongLength: 294000, SongImage: "musen/songs/covers/wetland.jpg"
+    songId: 11, songLink: "musen/songs/lofi/mist.mp3",
+    songTitle: "MIST", songArtist: "吉村弘",
+    songLength: 294000, songImage: "musen/songs/covers/wetland.jpg"
 	},
 	/*{
-    SongLink: "musen/songs/lofi/kawamo.mp3",
-    SongTitle: "かわも", SongArtist: "吉村弘",
-    SongLength: 250000, SongImage: "musen/songs/covers/wetland.jpg"
+    songLink: "musen/songs/lofi/kawamo.mp3",
+    songTitle: "かわも", songArtist: "吉村弘",
+    songLength: 250000, songImage: "musen/songs/covers/wetland.jpg"
 	},*/
 	{
-    SongId: 12, SongLink: "musen/songs/jazz/rush.mp3",
-    SongTitle: "RUSH", SongArtist: "THE SEATBELTS",
-    SongLength: 215000, SongImage: "musen/songs/covers/bebop.jpg"
+    songId: 12, songLink: "musen/songs/jazz/rush.mp3",
+    songTitle: "RUSH", songArtist: "THE SEATBELTS",
+    songLength: 215000, songImage: "musen/songs/covers/bebop.jpg"
 	},
 	{
-    SongId: 13, SongLink: "musen/songs/citypop/joke.mp3",
-    SongTitle: "JUST A JOKE", SongArtist: "YURIE KOKUBU",
-    SongLength: 180000, SongImage: "musen/songs/covers/relief.jpg"
+    songId: 13, songLink: "musen/songs/citypop/joke.mp3",
+    songTitle: "JUST A JOKE", songArtist: "YURIE KOKUBU",
+    songLength: 180000, songImage: "musen/songs/covers/relief.jpg"
 	},
 	{
-    SongId: 14, SongLink: "musen/songs/citypop/shuffle.mp3",
-    SongTitle: "MIDNIGHT SHUFFLE", SongArtist: "COSMOS",
-    SongLength: 295000, SongImage: "musen/songs/covers/cosmos.jpg"
+    songId: 14, songLink: "musen/songs/citypop/shuffle.mp3",
+    songTitle: "MIDNIGHT SHUFFLE", songArtist: "COSMOS",
+    songLength: 295000, songImage: "musen/songs/covers/cosmos.jpg"
 	},
 	{
-    SongId: 15, SongLink: "musen/songs/lofi/ole.mp3",
-    SongTitle: "OLE", SongArtist: "FAT JON",
-    SongLength: 208000, SongImage: "musen/songs/covers/champloo.jpg"
+    songId: 15, songLink: "musen/songs/lofi/ole.mp3",
+    songTitle: "OLE", songArtist: "FAT JON",
+    songLength: 208000, songImage: "musen/songs/covers/champloo.jpg"
 	},
 	{
-    SongId: 16, SongLink: "musen/songs/lofi/justforget.mp3",
-    SongTitle: "JUST FORGET", SongArtist: "FORCE OF NATURE",
-    SongLength: 231000, SongImage: "musen/songs/covers/champloo.jpg"
+    songId: 16, songLink: "musen/songs/lofi/justforget.mp3",
+    songTitle: "JUST FORGET", songArtist: "FORCE OF NATURE",
+    songLength: 231000, songImage: "musen/songs/covers/champloo.jpg"
 	},
 	{
-    SongId: 17, SongLink: "musen/songs/lofi/process.mp3",
-    SongTitle: "PROCESS", SongArtist: "TSUTCHIE",
-    SongLength: 281000, SongImage: "musen/songs/covers/champloo.jpg"
+    songId: 17, songLink: "musen/songs/lofi/process.mp3",
+    songTitle: "PROCESS", songArtist: "TSUTCHIE",
+    songLength: 281000, songImage: "musen/songs/covers/champloo.jpg"
 	},
 	{
-    SongId: 18, SongLink: "musen/songs/jazz/adrenaline.mp3",
-    SongTitle: "ADRENALINE", SongArtist: "INDIGO JAM UNIT",
-    SongLength: 269000, SongImage: "musen/songs/covers/realism.webp"
+    songId: 18, songLink: "musen/songs/jazz/adrenaline.mp3",
+    songTitle: "ADRENALINE", songArtist: "INDIGO JAM UNIT",
+    songLength: 269000, songImage: "musen/songs/covers/realism.webp"
 	},
 	{
-    SongId: 19, SongLink: "musen/songs/jazz/breeze.mp3",
-    SongTitle: "BREEZE", SongArtist: "JIRO INAGAKI & SOUL MEDIA",
-    SongLength: 278000, SongImage: "musen/songs/covers/funkystuff.webp"
+    songId: 19, songLink: "musen/songs/jazz/breeze.mp3",
+    songTitle: "BREEZE", songArtist: "JIRO INAGAKI & SOUL MEDIA",
+    songLength: 278000, songImage: "musen/songs/covers/funkystuff.webp"
 	},
 	{
-    SongId: 20, SongLink: "musen/songs/jazz/qualia.mp3",
-    SongTitle: "QUALIA", SongArtist: "PRIMITIVE ART ORCHESTRA",
-    SongLength: 233000, SongImage: "musen/songs/covers/qualia.jpg"
+    songId: 20, songLink: "musen/songs/jazz/qualia.mp3",
+    songTitle: "QUALIA", songArtist: "PRIMITIVE ART ORCHESTRA",
+    songLength: 233000, songImage: "musen/songs/covers/qualia.jpg"
 	},
 	{
-    SongId: 21, SongLink: "musen/songs/jazz/fragile.mp3",
-    SongTitle: "FRAGILE", SongArtist: "中村春香",
-    SongLength: 162000, SongImage: "musen/songs/covers/fragile.jpg"
+    songId: 21, songLink: "musen/songs/jazz/fragile.mp3",
+    songTitle: "FRAGILE", songArtist: "中村春香",
+    songLength: 162000, songImage: "musen/songs/covers/fragile.jpg"
 	},
 	{
-    SongId: 22, SongLink: "musen/songs/indie/passion.mp3",
-    SongTitle: "PASSION", SongArtist: "KEYTALK",
-    SongLength: 223000, SongImage: "musen/songs/covers/sugar.jpg"
+    songId: 22, songLink: "musen/songs/indie/passion.mp3",
+    songTitle: "PASSION", songArtist: "KEYTALK",
+    songLength: 223000, songImage: "musen/songs/covers/sugar.jpg"
 	},
 	{
-    SongId: 23, SongLink: "musen/songs/indie/bond.mp3",
-    SongTitle: "BOND", SongArtist: "LITE",
-    SongLength: 175000, SongImage: "musen/songs/covers/installation.jpg"
+    songId: 23, songLink: "musen/songs/indie/bond.mp3",
+    songTitle: "BOND", songArtist: "LITE",
+    songLength: 175000, songImage: "musen/songs/covers/installation.jpg"
 	},
 	{
-    SongId: 24, SongLink: "musen/songs/citypop/lostinparadise.mp3",
-    SongTitle: "LOST IN PARADISE", SongArtist: "ALI",
-    SongLength: 328000, SongImage: "musen/songs/covers/paradise.jpg"
+    songId: 24, songLink: "musen/songs/citypop/lostinparadise.mp3",
+    songTitle: "LOST IN PARADISE", songArtist: "ALI",
+    songLength: 328000, songImage: "musen/songs/covers/paradise.jpg"
 	}
 ]
 
--- index.js
+++ index.js
@@ -1,56 +1,72 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const express = require("express");
+const app = express();
+const http = require("http");
+const https = require("https");
+const WebSocket = require("ws");
+const fs = require("fs");
+
+// const fullchain = fs.readFileSync('/etc/letsencrypt/live/vfranco.me/fullchain.pem', 'utf8')
+// const privkey = fs.readFileSync('/etc/letsencrypt/live/vfranco.me/privkey.pem', 'utf8')
+// const credentials = {key: privkey, cert: fullchain};
 
 const port = 5940;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server });
+const path = "/info";
+// const server = https.createServer(credentials, app);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server, path: path });
 
-const {trackList} = require('./TrackList');
+const { trackList } = require("./TrackList");
 
 server.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
+  console.log(`Server is listening on port ${port}...`);
 });
 
-wss.on('connection', function connection(ws){
-    ws.on('message', function incoming(data){
-        wss.clients.forEach((client) => {
-            if (client.readyState == WebSocket.OPEN){
-                sendSong(client, currentSongPlaying)
-            }
-        });
-    })
-})
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(data) {
+    wss.clients.forEach((client) => {
+      if (client.readyState == WebSocket.OPEN) {
+        sendSong(client, currentSongPlaying);
+      }
+    });
+  });
+});
 
 var currentSongPlaying = "";
 let currentSongLength = 10000;
 let currentSongStarted = Date.now();
 
-getNewSong()
+getNewSong();
 
 function getNewSong() {
-	var counter = Math.floor(Math.random()*25)
-	var songInfo = trackList[counter]
-	console.log(`Next Song: ${ trackList[counter].SongTitle } - ${ trackList[counter].SongArtist }`)
-	currentSongPlaying = songInfo
-	currentSongLength = trackList[counter].SongLength
-	currentSongStarted = Date.now();
-	console.log(`Length: ${ currentSongLength }`)
+  var counter = Math.floor(Math.random() * 25);
+  var songInfo = trackList[counter];
+  console.log(
+    `Next Song: ${trackList[counter].songTitle} - ${trackList[counter].songArtist}`
+  );
+  currentSongPlaying = songInfo;
+  currentSongLength = trackList[counter].songLength;
+  currentSongStarted = Date.now();
+  console.log(`Length: ${currentSongLength}`);
 
-	wss.clients.forEach((client) => {
-      if (client.readyState == WebSocket.OPEN){
-          sendSong(client, currentSongPlaying)
-      }
+  wss.clients.forEach((client) => {
+    if (client.readyState == WebSocket.OPEN) {
+      sendSong(client, currentSongPlaying);
+    }
   });
 
-	setTimeout(getNewSong, currentSongLength)
-	console.log("New Song Picked!----------------------------------------------------------")
+  setTimeout(getNewSong, currentSongLength);
+  console.log(
+    "New Song Picked!----------------------------------------------------------"
+  );
 }
 
 //Send Song Currently Playing
-function sendSong(client, currentSongPlaying){
-	console.log(`Sending: ${ currentSongPlaying.SongTitle }`)
-	console.log(`Timestamp: ${ Date.now() - currentSongStarted }`)
-	let tempSong = JSON.stringify(currentSongPlaying)
-	client.send(tempSong.substring(0, tempSong.length - 1)+ `,"SongTimestamp":${ Date.now() - currentSongStarted }}`)
+function sendSong(client, currentSongPlaying) {
+  console.log(`Sending: ${currentSongPlaying.songTitle}`);
+  console.log(`Timestamp: ${Date.now() - currentSongStarted}`);
+  let tempSong = JSON.stringify(currentSongPlaying);
+  client.send(
+    tempSong.substring(0, tempSong.length - 1) +
+      `,"songTimestamp":${Date.now() - currentSongStarted}}`
+  );
 }
